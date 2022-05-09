cccd84ef0b1825e755c89bd5678609f98ff6f6e3
Yuya <ky199804096363@gmail.com>
2022-05-09 11:58:44 -0700

    done

--- js/portofolio.js
+++ js/portofolio.js
@@ -15,100 +15,125 @@ app.controller("appctr",function($scope,$timeout){
     var letter2;
     let count=0;
     const lettering=()=>{
-        if(count%2==0){
-            if(letter<array.name.length){
-                text+=array.name[letter];
-                $("#sutitling").html(text);
-                letter++;
-                setTimeout(lettering,150);
-            }
-            else if(letter==array.name.length){
-                letter++;
-                letter2=array.name.length;
-                setTimeout(lettering,1000);
-            }
-            else if(letter>array.name.length){
-                letter++;
-                if(letter2>0){
-                    text=text.slice(0,-1);
+        $(".index").css({opacity:"1"});
+        setTimeout(()=>{
+            $(".index").css({opacity:"0"});
+            if(count%2==0){
+                if(letter<array.name.length){
+                    text+=array.name[letter];
                     $("#sutitling").html(text);
-                    letter2--;
-                    setTimeout(lettering,100);
+                    letter++;
+                    setTimeout(lettering,150);
                 }
-                else{
-                    letter=0;
-                    count++;
+                else if(letter==array.name.length){
+                    letter++;
+                    letter2=array.name.length;
                     setTimeout(lettering,1000);
                 }
-            }
-        }
-        else{
-            if(letter<array.job.length){
-                text+=array.job[letter];
-                $("#sutitling").html(text);
-                letter++;
-                setTimeout(lettering,150);
-            }
-            else if(letter==array.job.length){
-                letter++;
-                letter2=array.job.length;
-                setTimeout(lettering,1000);
-            }
-            else if(letter>array.job.length){
-                letter++;
-                if(letter2>0){
-                    text=text.slice(0,-1);
-                    $("#sutitling").html(text);
-                    letter2--;
-                    setTimeout(lettering,100);
+                else if(letter>array.name.length){
+                    letter++;
+                    if(letter2>0){
+                        text=text.slice(0,-1);
+                        $("#sutitling").html(text);
+                        letter2--;
+                        setTimeout(lettering,100);
+                    }
+                    else{
+                        letter=0;
+                        count++;
+                        setTimeout(lettering,1000);
+                    }
                 }
-                else{
-                    letter=0;
-                    count++;
+            }
+            else{
+                if(letter<array.job.length){
+                    text+=array.job[letter];
+                    $("#sutitling").html(text);
+                    letter++;
+                    setTimeout(lettering,150);
+                }
+                else if(letter==array.job.length){
+                    letter++;
+                    letter2=array.job.length;
                     setTimeout(lettering,1000);
                 }
+                else if(letter>array.job.length){
+                    letter++;
+                    if(letter2>0){
+                        text=text.slice(0,-1);
+                        $("#sutitling").html(text);
+                        letter2--;
+                        setTimeout(lettering,100);
+                    }
+                    else{
+                        letter=0;
+                        count++;
+                        setTimeout(lettering,1000);
+                    }
+                }
             }
-        }
- 
+        },100);
     }
 
     lettering();
 
     $("#nav_icon_drop_list_resume").hover(()=>{$("#popups").css("display","block");},()=>{$("#popups").css("display","none");});
-
     $(window).on("scroll",function(){
         if($(document).scrollTop()>$("#biography").offset().top){ 
             $("#nav_icon").show(1000);
         }
         if($(document).scrollTop()<$("#biography").offset().top){ 
             $("#nav_icon").hide(1000);
+            $("#nav_icon_drop").hide(500);
         }
 
         if($(document).scrollTop()>$("#BIO").offset().top-200){ 
             $(".bio").animate({"top":"0","opacity":"1"},1000);
+            $(".titles")[0].classList.add("togglingShow");
+            $("#bioContents")[0].classList.add("contentsShow");
+        }
+        if($(document).scrollTop()<$("#BIO").offset().top-200){ 
+            $(".titles")[0].classList.remove("togglingShow");
+            $("#bioContents")[0].classList.remove("contentsShow");
         }
         
         if($(document).scrollTop()>$("#firstSectionH1").offset().top-200){ 
             $(".firstSectionElements").animate({"left":"0","opacity":"1"},1000);
+            $(".titles")[1].classList.add("togglingShow");
+        }
+
+        if($(document).scrollTop()<$("#firstSectionH1").offset().top-200){ 
+            $(".titles")[1].classList.remove("togglingShow");
         }
         
         if($(document).scrollTop()>$("#secondSectionH1").offset().top-200){ 
             $(".secondSectionElements").animate({"opacity":"1"},1000);
+            $(".titles")[2].classList.add("togglingShow");
+        }
+        if($(document).scrollTop()<$("#secondSectionH1").offset().top-200){ 
+            $(".titles")[2].classList.remove("togglingShow");
         }
         
         if($(document).scrollTop()>$("#thirdSectionH1").offset().top-200){ 
             $(".thirdSectionElements i").animate({"opacity":"1"},1000);
+            $(".titles")[3].classList.add("togglingShow");
+        }
+        if($(document).scrollTop()<$("#thirdSectionH1").offset().top-200){ 
+            $(".titles")[3].classList.remove("togglingShow");
         }
 
-        if($(document).scrollTop()>$("footer").offset().top-300){ 
-            $(".sns").animate({"bottom":"0","opacity":"1"},1000);
+        if($(document).scrollTop()+$(window).height()>$("footer").offset().top+50){ 
+            $("#sns")[0].classList.add("ani");
+            console.log( $("#sns")[0]);
+        }
+        if($(document).scrollTop()+$(window).height()<$("footer").offset().top+50){ 
+            $("#sns")[0].classList.remove("ani");
         }
 
     });
 
+
     $(window).on("click",(e)=>{
-        console.log(e.target);
-       
         let close_count=0;
       
         if(e.target==$("#nav_icon_img")[0]){
@@ -140,44 +165,57 @@ app.controller("appctr",function($scope,$timeout){
         }  
         for(let i=0;i<$(".project_img").length;i++){
             if(close_pro==0 && e.target!=$(".project_img")[i]){
-                console.log("asas");
                 $("#project_model").hide();
             }
             else if(e.target==$(".project_img")[i]){
-                console.log("das");
                 $("#project_model").show();
                 $("#projects").show();
         
                 $(".project_dashboard").hide();
                 $(".project_bakery").hide();
                 $(".project_register").hide();
+                $(".project_blog").hide();
                 
                 if(e.target.id=="img_dash"){
                     $(".project_dashboard").show();
                     if($(window).width()<825){
-                        $("#project_model").css("top","0");
+                        $("#project_model").css("top",`${dash}px`);
                     }
                     else{
                         $("#project_model").css("top","0");
                     }
+                    $(document).scrollTop($("#dash").offset().top);
                 }
                 else if(e.target.id=="img_bakery"){
                     $(".project_bakery").show();
                     if($(window).width()<825){
-                        $("#project_model").css("top","200px");
+                        $("#project_model").css("top",`${bakery}px`);
                     }
                     else{
                         $("#project_model").css("top","0");
                     }
+                    $(document).scrollTop($("#bakery").offset().top);
+
                 }
                 else if(e.target.id=="img_login"){
                     $(".project_register").show();
                     if($(window).width()<825){
-                        $("#project_model").css("top","500px");
+                        $("#project_model").css("top",`${login}px`);
                     }
                     else{
                         $("#project_model").css("top","0");
                     }
+                    $(document).scrollTop($("#login").offset().top);
+                }
+                else if(e.target.id=="img_blog"){
+                    $(".project_blog").show();
+                    if($(window).width()<825){
+                        $("#project_model").css("top",`${blog}px`);
+                    }
+                    else{
+                        $("#project_model").css("top","0");
+                    }
+                    $(document).scrollTop($("#blog").offset().top);
                 }
                 break;
             }
@@ -187,5 +225,12 @@ app.controller("appctr",function($scope,$timeout){
     $("#hide_model").on("click",function(){
         $("#project_model").hide();
     });
+
+    const dash=$("#dash").offset().top-$("#model_contents").offset().top;
+    const bakery=$("#bakery").offset().top-$("#model_contents").offset().top;
+    const login=$("#login").offset().top-$("#model_contents").offset().top;
+    const blog=$("#blog").offset().top-$("#model_contents").offset().top;
+
 });
+
 
